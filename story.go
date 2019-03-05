a35835faad0f747a619ce1c142750361f0146bd0
Alankrit Joshi <alankritjoshi@gmail.com>
2019-03-04 20:44:18 -0800

    Working story

--- story.go
+++ story.go
@@ -2,14 +2,68 @@ package cyoa
 
 import (
 	"encoding/json"
+	"html/template"
 	"io"
-
-	"github.com/alankritjoshi/cyoa"
+	"log"
+	"net/http"
+	"strings"
 )
 
+func init() {
+	tpl = template.Must(template.New("").Parse(defaultHandlerTmpl))
+}
+
+var tpl *template.Template
+
+var defaultHandlerTmpl = `
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Choose Your Own Adventure</title>
+</head>
+<body>
+    <h1>{{.Title}}</h1>
+    {{range .Paragraphs}}
+    <p>{{.}}</p>
+    {{end}}
+    <ul>
+        {{range .Options}}
+        <li><a href="/{{.Chapter}}">{{.Text}}</a></li>
+        {{end}}
+    </ul>
+</body>
+</html>`
+
+func NewHandler(s Story) http.Handler {
+	return handler{s}
+}
+
+type handler struct {
+	s Story
+}
+
+func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	path := strings.TrimSpace(r.URL.Path)
+	if path == "" || path == "/" {
+		path = "/intro"
+	}
+	path = path[1:]
+
+	if chapter, ok := h.s[path]; ok {
+		err := tpl.Execute(w, chapter)
+		if err != nil {
+			log.Printf("%v", err)
+			http.Error(w, "Something went wrong...", http.StatusInternalServerError)
+		}
+		return
+	}
+	http.Error(w, "Chapter not found.", http.StatusNotFound)
+}
+
 func JsonStory(r io.Reader) (Story, error) {
-	d := json.NewDecoder(f)
-	var story cyoa.Story
+	d := json.NewDecoder(r)
+	var story Story
 	if err := d.Decode(&story); err != nil {
 		return nil, err
 	}
